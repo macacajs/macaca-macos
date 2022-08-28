89de0726fe66881458f39e7b706224494f8d0b7d
xudafeng <xudafeng@126.com>
2022-08-29 00:18:20 +0800

    docs: update jsdoc

--- src/driver/clipboard.ts
+++ src/driver/clipboard.ts
@@ -1,10 +1,19 @@
 import { jxaUtil } from '../jxa/jxaUtil';
 
 export default class ClipboardDriver {
+  /**
+   * 获取剪切板的文本
+   * @returns string
+   */
   async getClipText() {
     return jxaUtil.getClipText();
   }
 
+  /**
+   * 设置文本到剪切板
+   * @param str string
+   * @returns string
+   */
   async setClipText(str: string) {
     return jxaUtil.setClipText(str);
   }
