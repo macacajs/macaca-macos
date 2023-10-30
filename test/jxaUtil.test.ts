3291f773d021374a39d1aecf4673c28852511fd9
yihuineng <471110230@qq.com>
2023-10-30 13:34:46 +0800

    chore: x64 binary

--- test/jxaUtil.test.ts
+++ test/jxaUtil.test.ts
@@ -1,14 +1,8 @@
 import { jxaUtil } from '../src/core/jxa/jxaUtil';
 import MacacaMacOS from '../src/macaca-macos';
 import assert from 'assert';
-import os from 'os';
 
 describe.skip('jxaUtil unit testing', function() {
-
-  it('console arch', async function() {
-    this.timeout(0);
-    console.log(os.arch());
-  });
 
   it('focusApp should be ok', async () => {
     await jxaUtil.focusApp('Notes');
